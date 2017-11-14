4b6dea52e40506c1ec8fcb44a8d4ed76bf82b7af
Lance-Abais <ljda07@yahoo.com>
2017-11-14 20:21:13 +0800

    Skelly UI

--- app/src/main/java/edu/ateneo/cie199/bucketdish/CommentRatingActivity.java
+++ app/src/main/java/edu/ateneo/cie199/bucketdish/CommentRatingActivity.java
@@ -3,11 +3,11 @@ package edu.ateneo.cie199.bucketdish;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CommentRatingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_comment_rating);
     }
 }
--- app/src/main/java/edu/ateneo/cie199/bucketdish/SearchResultActivity.java
+++ app/src/main/java/edu/ateneo/cie199/bucketdish/SearchResultActivity.java
@@ -3,11 +3,11 @@ package edu.ateneo.cie199.bucketdish;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search_result);
     }
 }
