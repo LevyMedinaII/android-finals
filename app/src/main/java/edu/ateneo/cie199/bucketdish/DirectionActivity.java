e87e1e3c842e84864829c67aad02be2eac2dac9b
Lance-Abais <ljda07@yahoo.com>
2017-12-07 23:59:09 +0800

    Cleaned Direction Activity and removed direction json parser class as well as implementations for routing.

--- app/src/main/java/edu/ateneo/cie199/bucketdish/DirectionActivity.java
+++ app/src/main/java/edu/ateneo/cie199/bucketdish/DirectionActivity.java
@@ -52,16 +52,6 @@ public class DirectionActivity extends FragmentActivity implements OnMapReadyCal
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
@@ -78,12 +68,15 @@ public class DirectionActivity extends FragmentActivity implements OnMapReadyCal
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        //shows user location and tracks
         mMap.setMyLocationEnabled(true);
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
+        //adds a marker at the location of the resto with a title of the resto name
         mMap.addMarker(new MarkerOptions().position(getRestoLocation()).title(restoName));
     }
 
+    //a function that returns the Latitude Longitude of the position of the user in a LatLng object
     private LatLng getUserLocation(){
 
         Intent getUserLatLng = getIntent();
@@ -95,6 +88,7 @@ public class DirectionActivity extends FragmentActivity implements OnMapReadyCal
 
     }
 
+    //a function that returns the Latitude Longitude of the position of the resto in a LatLng object
     private LatLng getRestoLocation(){
         Intent getRestoLatLng = getIntent();
         Double restoLat = getRestoLatLng.getDoubleExtra("restolat",0);
@@ -117,135 +111,4 @@ public class DirectionActivity extends FragmentActivity implements OnMapReadyCal
     }
 
 
-    private class DownloadTask extends AsyncTask<String,String,String>{
-
-        @Override
-        protected void onPostExecute(String url) {
-            super.onPostExecute(url);
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            String data = "";
-            try {
-                data = downloadUrl(strings[0]);
-            } catch (IOException e) {
-                Log.d("Background Task", e.toString());
-            }
-            return null;
-        }
     }
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
-
-        // Parsing the data in non-ui thread
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try {
-                jObject = new JSONObject(jsonData[0]);
-                DirectionsJSONParser parser = new DirectionsJSONParser();
-
-                routes = parser.parse(jObject);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList points = null;
-            PolylineOptions lineOptions = null;
-            MarkerOptions markerOptions = new MarkerOptions();
-
-            for (int i = 0; i < result.size(); i++) {
-                lineOptions = new PolylineOptions();
-
-                List<HashMap<String, String>> path = result.get(i);
-
-                for (int j = 0; j < path.size(); j++) {
-                    points = new ArrayList();
-                    HashMap point = path.get(j);
-
-                    double lat = Double.parseDouble((String) point.get("lat"));
-                    double lng = Double.parseDouble((String) point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                lineOptions.addAll(points);
-                lineOptions.width(12);
-                lineOptions.color(Color.RED);
-                lineOptions.geodesic(true);
-
-            }
-
-// Drawing polyline in the Google Map for the i-th route
-            mMap.addPolyline(lineOptions);
-        }
-    }
-
-    private String getDirectionsUrl(LatLng origin, LatLng dest) {
-
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-
-        // Sensor enabled
-        String sensor = "sensor=false";
-        String mode = "mode=driving";
-
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&" + mode;
-
-        // Output format
-        String output = "json";
-
-        // Building the url to the web service
-        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
-
-
-        return url;
-    }
-
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            urlConnection.connect();
-
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb = new StringBuffer();
-
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            data = sb.toString();
-
-            br.close();
-
-        } catch (Exception e) {
-            Log.d("Exception", e.toString());
-        } finally {
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-}
